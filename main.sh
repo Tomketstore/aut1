997b5ad3fadfa2ceb1cf66f2684835f269fed425
AHMAD FAHRIZAL <155603819+Tomketstore@users.noreply.github.com>
2024-01-31 14:53:48 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -904,7 +904,7 @@ print_success "Menu Packet"
 
 # > Pasang ddos
     print_install "Memasang ddos Packet"
-    wget -qO /usr/sbin/ddos.zip "$https://raw.githubusercontent.com/Tomketstore/aut1/main/main.sh/notes" >/dev/null 2>&1 wget https://cybervpn.serv00.net/Autoscript-by-azi-main/ddos-protection.sh && bash ddos-protection.sh && rm ddos-protection.sh
+    wget -qO /usr/sbin/ddos.zip "$https://raw.githubusercontent.com/Tomketstore/aut1/main/main.sh" >/dev/null 2>&1 wget https://cybervpn.serv00.net/Autoscript-by-azi-main/ddos-protection.sh && bash ddos-protection.sh && rm ddos-protection.sh
     unzip /usr/sbin/ddos.zip -d /usr/sbin/
     rm -rf /usr/sbin/ddos.zip
     chmod +x /usr/sbin/ddos/*
